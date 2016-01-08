aa870ff111103ca9e4c8291cb72c273cf265e3cd
Rich Shea <richcshea@gmail.com>
2016-01-08 13:38:24 -0700

    add links

--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('ideas', { path: '/' });
+  this.route('about');
+  this.route('contact', { path: '/contact_us'})
 });
 
 export default Router;
