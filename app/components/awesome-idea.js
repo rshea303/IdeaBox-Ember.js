2fb352ad32e8592abcdf7afadcd619ebda96f376
Rich Shea <richcshea@gmail.com>
2016-01-08 11:00:51 -0700

    add edit button

--- app/components/awesome-idea.js
+++ app/components/awesome-idea.js
@@ -2,7 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  editing: false,
+
   actions: {
+    edit: function() {
+      let editing = this.get('editing');
+      if (editing) { 
+        this.get('idea').save().then( () => {
+          this.set('editing', false);
+        });
+      
+      }
+      this.set('editing', !editing);  
+    },
+
     delete: function() {
       this.get('idea').destroyRecord(); 
     }
